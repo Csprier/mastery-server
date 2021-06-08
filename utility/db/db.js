60e8cfb7d83abfad0e7d1b4c70f07ec469eb9f87
Csprier <csprier@gmail.com>
2021-06-08 14:41:45 -0400

    altered db.js temporarily to bypass connection issues

--- utility/db/db.js
+++ utility/db/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
 
+// const db = mysql.createPool({
+//   host: process.env.MASTERY_DATABASE || '172.21.0.2',
+// 	user: process.env.MYSQL_USER || 'wiz',
+// 	password: process.env.MYSQL_PASSWORD || 'cloak',
+// 	database: process.env.MASTERY_DATABASE || 'mastery-db',
+// });
+
 const db = mysql.createPool({
-  host: process.env.MASTERY_DATABASE || '172.21.0.2',
-	user: process.env.MYSQL_USER || 'wiz',
-	password: process.env.MYSQL_PASSWORD || 'cloak',
-	database: process.env.MASTERY_DATABASE || 'mastery-db',
+  host: 'localhost',
+	user: 'wiz',
+	password: 'cloak',
+	database: 'mastery-db',
 });
 
 module.exports = db;
