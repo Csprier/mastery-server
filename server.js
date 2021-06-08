ea36445d902d609949fece558d78d0b0bb6a41d0
Csprier <csprier@gmail.com>
2021-06-08 14:59:09 -0400

    Added a console.log to /gathering endpoint

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.get('/gathering', (req, res) => {
   db.query('SELECT * FROM GatheringBasicItems', (err, data, fields) => {
     if (err) throw error;
     res.json(data);
+		console.log(data);
   });
 });
 
