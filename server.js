a8e05228e8e3d9eeeb5fb8bf74dce07ed1c937b7
Csprier <csprier@gmail.com>
2021-06-21 17:28:24 -0400

    added alchemy route to server.js temporarily

--- server.js
+++ server.js
@@ -137,4 +137,17 @@ app.get('/training', (req, res) => {
 	});
 });
 
+/** ====================================================================/ 
+ * ALCHEMY ROUTES 
+ * [] - (need to be moved to their own file eventually) 
+ * =============================================/ 
+*/
+app.get('/alchemy', (req, res) => {
+	db.query('SELECT * FROM alchemy_data', (err, data) => {
+		if (err) throw error;
+		res.json(data);
+		// console.log(data);
+	});
+});
+
 app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}!`));
