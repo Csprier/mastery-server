d3351ad41e7db659bf25e0201eb97349042502fa
Csprier <csprier@gmail.com>
2021-07-08 11:11:52 -0400

    Removed comments from alchemy.routes.js

--- routes/alchemy.routes.js
+++ routes/alchemy.routes.js
@@ -3,16 +3,10 @@ const db = require('../utility/db/db.js');
 
 const router = express.Router();
 
-/** ====================================================================/ 
- * ALCHEMY ROUTES 
- * [] - (need to be moved to their own file eventually) 
- * =============================================/ 
-*/
 app.get('/alchemy', (req, res) => {
 	db.query('SELECT * FROM alchemy_data', (err, data) => {
 		if (err) throw error;
 		res.json(data);
-		// console.log(data);
 	});
 });
 
