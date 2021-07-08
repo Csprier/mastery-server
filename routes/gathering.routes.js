a186aaea4601bba3dabb3cbc8252fabefe5c479e
Csprier <csprier@gmail.com>
2021-07-08 12:25:26 -0400

    Adjusted gathering.routes.js to mirror alchemy.routes IIFE format

--- routes/gathering.routes.js
+++ routes/gathering.routes.js
@@ -1,42 +1,38 @@
-const express = require('express');
-const db = require('../utility/db/db.js');
-
-const router = express.Router();
-
-router.get('/gathering', (req, res) => {
-  db.query('SELECT * FROM GatheringBasicItems', (err, data, fields) => {
-    if (err) throw error;
-    res.send(data);
+module.exports = (function() {
+  const gatheringRouter = require('express').Router();
+  const db = require('../utility/db/db');
+  gatheringRouter.get('/gathering', (req, res) => {
+    db.query('SELECT * FROM gathering_data', (err, data, fields) => {
+      if (err) throw error;
+      res.send(data);
+    });
   });
-});
-
-router.get('/gathering/basic-items', (req, res) => {
-  db.query('SELECT * FROM GatheringBasicItems', (err, data, fields) => {
-    if (err) throw error;
-    res.json(data);
+  gatheringRouter.get('/gathering/basic-items', (req, res) => {
+    db.query('SELECT * FROM GatheringBasicItems', (err, data, fields) => {
+      if (err) throw error;
+      res.json(data);
+    });
   });
-});
+  gatheringRouter.get('/gathering/rare-resources', (req, res) => {
+    db.query('SELECT * FROM GatheringRareResources', (err, data, fields) => {
+      if (err) throw error;
+      res.json(data);
+    })
+  });
+  
+  gatheringRouter.get('/gathering/special-resources', (req, res) => {
+    db.query('SELECT * FROM GatheringSpecialResources', (err, data, fields) => {
+      if (err) throw error;
+      res.json(data);
+    })
+  });
+  gatheringRouter.get('/gathering/very-rare-resources', (req, res) => {
+    db.query('SELECT * FROM GatheringVeryRareResources', (err, data, fields) => {
+      if (err) throw error;
+      res.json(data);
+    })
+  });
 
-router.get('/gathering/rare-resources', (req, res) => {
-	db.query('SELECT * FROM GatheringRareResources', (err, data, fields) => {
-		if (err) throw error;
-    res.json(data);
-	})
-});
-
-router.get('/gathering/special-resources', (req, res) => {
-	db.query('SELECT * FROM GatheringSpecialResources', (err, data, fields) => {
-		if (err) throw error;
-		res.json(data);
-	})
-});
-
-router.get('/gathering/very-rare-resources', (req, res) => {
-	db.query('SELECT * FROM GatheringVeryRareResources', (err, data, fields) => {
-		if (err) throw error;
-    res.json(data);
-	})
-});
-
-module.exports = router;
+  return gatheringRouter;
+})();
 
