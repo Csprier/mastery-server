188359044613149d449fa19952beb2e12b78c227
Csprier <csprier@gmail.com>
2021-07-15 14:52:34 -0400

    Changed line 50 in server.js to docker connection config to test docker-compose build

--- server.js
+++ server.js
@@ -47,4 +47,4 @@ app.get('/greeting', (req, res) => {
 	res.send(res);
 });
 
-app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}!`));
+app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}!`));
