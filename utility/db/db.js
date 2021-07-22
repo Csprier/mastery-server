4a75ccc9448b794d3872e9831a856692c4807e6f
Csprier <csprier@gmail.com>
2021-07-22 17:48:32 -0400

    Fixed host in createPool in db.js. Can now successfull docker-compose and seed database and access data from the client!

--- utility/db/db.js
+++ utility/db/db.js
@@ -1,18 +1,10 @@
 const mysql = require('mysql');
 
-// const db = mysql.createPool({
-//   host: process.env.MASTERY_DATABASE || '172.21.0.2',
-// 	user: process.env.MYSQL_USER || 'wiz',
-// 	password: process.env.MYSQL_PASSWORD || 'cloak',
-// 	database: process.env.MASTERY_DATABASE || 'mastery-db',
-// });
-
 const db = mysql.createPool({
-  host: 'localhost',
-	// host: '0.0.0.0',
+	host: 'masterydb',
 	user: 'wiz',
 	password: 'cloak',
-	database: 'mastery-db',
+	database: 'masterydb',
 });
 
 module.exports = db;
